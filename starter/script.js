230244e18b3cde7cebb68ecfebfc6f11cfe19196
Ramon Muñoz <ramonm2811@gmail.com>
2021-11-08 08:46:36 -0300

    classes

--- starter/script.js
+++ starter/script.js
@@ -3,6 +3,11 @@
 /* Seleccionamos a TODOS los botones con la clase .show-modal con el querySelectorAll, creando un array
 en la variable buttons, de esta manera podemos agregarle un eventListener a todos los elementos con el for loop */
 const buttons = document.querySelectorAll('.show-modal');
+
+//Guardamos en una variable los elementos con la clase .hidden
+const hidden = document.querySelectorAll('.hidden');
+
+//Le agregamos eventListener a todos los elementos de buttons
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', showModal);
 }
@@ -10,13 +15,15 @@ for (let i = 0; i < buttons.length; i++) {
 /* Seleccionamos a TODOS los elementos HTML con la clase .hidden con querySelectorAll creando un array ejn la variable hidden, luego 
 con un for loop le agregamos los métodos style a cada unos de los elementos. */
 function showModal() {
-  const hidden = document.querySelectorAll('.hidden');
   for (let i = 0; i < hidden.length; i++) {
-    hidden[i].style.display = 'inline-block';
+    hidden[i].classList.remove('hidden');
   }
-  document.querySelector('.close-modal').addEventListener('click', function () {
-    for (let i = 0; i < hidden.length; i++) {
-      hidden[i].style.display = 'none';
-    }
-  });
+  document.querySelector('.close-modal').addEventListener('click', closeModal);
+  document.querySelector('.overlay').addEventListener('click', closeModal);
+}
+
+function closeModal() {
+  for (let i = 0; i < hidden.length; i++) {
+    hidden[i].classList.add('hidden');
+  }
 }
